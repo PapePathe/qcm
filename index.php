68cc5b9912cf5f026480ae98787e8a2246b50f9b
Pathé SENE <pathe.sene@gmail.com>
2013-12-03 13:50:04 +0000

    Changement de position du formulaire creer question qui vient maintenant a gauche.
    On decoche le checkbox quand on clone une reponse dans le formulaire creer
    question.

--- index.php
+++ index.php
@@ -87,63 +87,7 @@
             <?php endif ?>
                 
             <div class="row">
-
-                <div class="col-md-7">
-
-                    <table class="table table-hover table-bordered">
-                        <thead>
-                            <tr>
-                                <th>Questions</th>
-                                <th>Réponses</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <?php
-                            
-                            $serverList = array('localhost', '127.0.0.1');
-                            if(in_array($_SERVER['HTTP_HOST'], $serverList))
-                            {                            
-                                // connexion et selection de la base
-                                mysql_connect("localhost", "root", "");
-                                mysql_select_db("qcm");
-                            }
-                            else
-                            {                            
-                                // connexion et selection de la base
-                                mysql_connect("localhost", "lagenced_stage", "quizz@3w");
-                                mysql_select_db("lagenced_stage");
-                            }
-                            
-                            
-                            $questions = mysql_query("select * from questions");
-                            if ($questions) {
-                                while($question = mysql_fetch_assoc($questions))
-                                {
-                                    
-                                    
-                                    $tr = "<tr><td>".$question['libelle']."</td><td><ul>";
-                                    
-                                    $select_reponses = mysql_query("select * from reponses where id_question=".$question['id']);
-                                    
-                                    if ($select_reponses) {
-                                        while ($reponse = mysql_fetch_assoc($select_reponses)) {
-                                            $label = $reponse['correcte'] == 0 ? 'danger' : 'success';
-                                            $tr .= "<li><span class='label label-$label'>". $reponse['libelle'] ."</span></li>";
-                                        }
-                                    }
-                                    
-                                    $tr .= "</ul></td><td> <a href='supprimer-question.php?id=".$question['id']."'>supprimer</a></td></tr>";
-                                    
-                                    echo $tr;
-                                }    
-                            }
-                            
-                        
-                        ?>
-                    </table>
-
-                </div>
-                <div class="col-md-5">
+                <div class="col-md-4">
 
                     <form role='form' method="POST" action="index.php">
                         <div class="panel panel-default">
@@ -199,6 +143,63 @@
                         </div>                        
                     </form>
                 </div>
+                <div class="col-md-8">
+
+                    <table class="table table-hover table-bordered table-condensed">
+                        <thead>
+                            <tr>
+                                <th>Questions</th>
+                                <th>Réponses</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <?php
+                            
+                            $serverList = array('localhost', '127.0.0.1');
+                            if(in_array($_SERVER['HTTP_HOST'], $serverList))
+                            {                            
+                                // connexion et selection de la base
+                                mysql_connect("localhost", "root", "");
+                                mysql_select_db("qcm");
+                            }
+                            else
+                            {                            
+                                // connexion et selection de la base
+                                mysql_connect("localhost", "lagenced_stage", "quizz@3w");
+                                mysql_select_db("lagenced_stage");
+                            }
+                            
+                            
+                            $questions = mysql_query("select * from questions");
+                            if ($questions) {
+                                while($question = mysql_fetch_assoc($questions))
+                                {
+                                    
+                                    
+                                    $tr = "<tr><td>".$question['libelle']."</td><td><ul>";
+                                    
+                                    $select_reponses = mysql_query("select * from reponses where id_question=".$question['id']);
+                                    
+                                    if ($select_reponses) {
+                                        while ($reponse = mysql_fetch_assoc($select_reponses)) {
+                                            $label = $reponse['correcte'] == 0 ? 'danger' : 'success';
+                                            $tr .= "<li><span class='label label-$label'>". $reponse['libelle'] ."</span></li>";
+                                        }
+                                    }
+                                    
+                                    $tr .= "</ul></td><td> <a href='supprimer-question.php?id=".$question['id']."'>supprimer</a></td></tr>";
+                                    
+                                    echo $tr;
+                                }    
+                            }
+                            
+                        
+                        ?>
+                    </table>
+
+                </div>
+                
+                
 
             </div>
 
--- js/init.js
+++ js/init.js
@@ -16,7 +16,8 @@ jQuery(document).ready(function(){
         * On change le nom et l'attribut for du champ label en y prefixant le nomre de réponses
         */
        $rep.children('label').text("Réponse "+$nombre_reponses).attr('for', "reponse_"+$nombre_reponses);       
-       $rep.children("input[type=text]").attr('id', "reponse_"+$nombre_reponses);
+       $rep.children("input[type=text]").attr('id', "reponse_"+$nombre_reponses).val('');     
+       $rep.find(".checkbox input[type=checkbox]").attr('checked', false);
        
        /**
         * On ajoute l'élément que l'on vient de cloner dans la liste des réponses
